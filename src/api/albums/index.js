d1b5a852601cd1c496cbb6d1ba002596879a65a8
nacxht <zombiecrafter069@gmail.com>
2024-07-11 21:33:52 +0700

    feat: register route, handler, and validator into plugin

--- src/api/albums/index.js
+++ src/api/albums/index.js
@@ -1,8 +1,12 @@
+import { AlbumsHandler } from './handlers.js'
+import { routes } from './routes.js'
+
 export const albumsPlugin = {
   name: 'albums',
   version: '1.0.0',
 
-  register: async (server, { attr }) => {
-    //
+  register: async (server, { service, validator }) => {
+    const albumsHandler = new AlbumsHandler(service, validator)
+    server.route(routes(albumsHandler))
   }
 }
--- src/api/songs/index.js
+++ src/api/songs/index.js
@@ -1,8 +1,12 @@
+import { SongsHandler } from './handlers.js'
+import { routes } from './routes.js'
+
 export const songsPlugin = {
   name: 'songs',
   version: '1.0.0',
 
-  register: async (server, { attr }) => {
-    //
+  register: async (server, { service, validator }) => {
+    const songsHandler = new SongsHandler(service, validator)
+    server.route(routes(songsHandler))
   }
 }
