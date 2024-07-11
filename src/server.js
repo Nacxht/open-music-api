e9d32d22734395156914dd2d546855e36eaf6ed0
nacxht <zombiecrafter069@gmail.com>
2024-07-11 21:34:18 +0700

    feat: register plugin

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import Hapi from '@hapi/hapi'
+import { albumsPlugin } from './api/albums/index.js'
+import { songsPlugin } from './api/songs/index.js'
+import { AlbumService } from './services/postgres/AlbumsService.js'
+import { SongsService } from './services/postgres/SongsService.js'
+import { albumsValidator } from './validators/albums/index.js'
+import { songsValidator } from './validators/songs/index.js'
 
 import dotenv from 'dotenv'
 dotenv.config()
 
 async function main () {
+  // Service instance
+  const albumsService = new AlbumService()
+  const songsService = new SongsService()
+
   // Create server
   const server = Hapi.server({
     port: process.env.PORT,
@@ -16,12 +26,25 @@ async function main () {
   })
 
   // Register plugins
-  await server.register({
-    //
-  })
+  await server.register([
+    {
+      plugin: albumsPlugin,
+      options: {
+        service: albumsService,
+        validator: albumsValidator
+      }
+    },
+    {
+      plugin: songsPlugin,
+      options: {
+        service: songsService,
+        validator: songsValidator
+      }
+    }
+  ])
 
   // PreResponse
-  server.ext('onPreRes    ponse', (request, h) => {
+  server.ext('onPreResponse', (request, h) => {
     //
   })
 
