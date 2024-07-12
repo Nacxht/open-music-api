ae41763784bef037a9ba38bc7ec1b3b6c70ceeef
nacxht <zombiecrafter069@gmail.com>
2024-07-12 23:18:05 +0700

    fix: fixed typo

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 import Hapi from '@hapi/hapi'
 import { albumsPlugin } from './api/albums/index.js'
 import { songsPlugin } from './api/songs/index.js'
-import { AlbumService } from './services/postgres/AlbumsService.js'
+import { AlbumsService } from './services/postgres/AlbumsService.js'
 import { SongsService } from './services/postgres/SongsService.js'
 import { albumsValidator } from './validators/albums/index.js'
 import { songsValidator } from './validators/songs/index.js'
 
 import dotenv from 'dotenv'
+import { ClientError } from './exceptions/ClientError.js'
 dotenv.config()
 
 async function main () {
   // Service instance
-  const albumsService = new AlbumService()
+  const albumsService = new AlbumsService()
   const songsService = new SongsService()
 
   // Create server
@@ -45,7 +46,18 @@ async function main () {
 
   // PreResponse
   server.ext('onPreResponse', (request, h) => {
-    //
+    const { response } = request
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message
+      }).code(response.statusCode)
+
+      return newResponse
+    }
+
+    return h.continue
   })
 
   // Start server
