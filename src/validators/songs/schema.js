1e3a4c3f3ffd6c1d7201aab0ce93b71676756356
nacxht <zombiecrafter069@gmail.com>
2024-07-12 23:22:54 +0700

    fix: change "year" property from string -> number

--- src/validators/songs/schema.js
+++ src/validators/songs/schema.js
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 export const songPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.string().required(),
+  year: Joi.number().required(),
   genre: Joi.string().required(),
   performer: Joi.string().required(),
   duration: Joi.number(),
