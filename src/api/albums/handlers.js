acd0084e98e36f77b4fa479c3eefa42b89daf151
nacxht <zombiecrafter069@gmail.com>
2024-07-12 23:18:38 +0700

    feat: add handler functionalities & register handler into routes

--- src/api/albums/handlers.js
+++ src/api/albums/handlers.js
@@ -6,19 +6,53 @@ export class AlbumsHandler {
     this.#validator = validator
   }
 
-  async postAlbumHandler () {
-    //
+  async postAlbumHandler (request, h) {
+    this.#validator.validateAlbumPayload(request.payload)
+    const albumId = await this.#service.addAlbum(request.payload)
+
+    const response = h.response({
+      status: 'success',
+      message: 'Album berhasil ditambahkan',
+      data: {
+        albumId
+      }
+    }).code(201)
+
+    return response
   }
 
-  async getAlbumByIdHandler () {
-    //
+  async getAlbumByIdHandler (request) {
+    const { id } = request.params
+    const album = await this.#service.getAlbumById(id)
+
+    return {
+      status: 'success',
+      data: {
+        album
+      }
+    }
   }
 
-  async putAlbumByIdHandler () {
-    //
+  async putAlbumByIdHandler (request) {
+    this.#validator.validateAlbumPayload(request.payload)
+    const { id } = await request.params
+
+    await this.#service.editAlbumById(id, request.payload)
+
+    return {
+      status: 'success',
+      message: 'Album berhasil diperbarui'
+    }
   }
 
-  async deleteAlbumByIdHandler () {
-    //
+  async deleteAlbumByIdHandler (request) {
+    const { id } = await request.params
+
+    await this.#service.deleteAlbumById(id)
+
+    return {
+      status: 'success',
+      message: 'Album berhasil dihapus'
+    }
   }
 }
--- src/api/songs/handlers.js
+++ src/api/songs/handlers.js
@@ -6,23 +6,65 @@ export class SongsHandler {
     this.#validator = validator
   }
 
-  async postSongHandler () {
-    //
+  async postSongHandler (request, h) {
+    this.#validator.validateSongPayload(request.payload)
+    const songId = await this.#service.addSong(request.payload)
+
+    const response = h.response({
+      status: 'success',
+      message: 'Berhasil menambahkan lagu',
+      data: {
+        songId
+      }
+    }).code(201)
+
+    return response
   }
 
-  async getAllSongsHandler () {
-    //
+  async getAllSongsHandler (request) {
+    const { title, performer } = request.query
+    const songs = await this.#service.getSongs(title, performer)
+
+    return {
+      status: 'success',
+      data: {
+        songs
+      }
+    }
   }
 
-  async getSongByIdHandler () {
-    //
+  async getSongByIdHandler (request) {
+    const { id } = request.params
+    const song = await this.#service.getSongById(id)
+
+    return {
+      status: 'success',
+      data: {
+        song
+      }
+    }
   }
 
-  async putSongByIdHandler () {
-    //
+  async putSongByIdHandler (request) {
+    this.#validator.validateSongPayload(request.payload)
+    const { id } = request.params
+
+    await this.#service.editSongById(id, request.payload)
+
+    return {
+      status: 'success',
+      message: 'Lagu berhasil diperbarui'
+    }
   }
 
-  async deleteSongByIdHandler () {
-    //
+  async deleteSongByIdHandler (request) {
+    const { id } = await request.params
+
+    await this.#service.deleteSongById(id)
+
+    return {
+      status: 'success',
+      message: 'Lagu berhasil dihapus'
+    }
   }
 }
