e1602d75120e150cd4c47201a23c1d6fb846e266
nacxht <zombiecrafter069@gmail.com>
2024-07-26 15:20:03 +0700

    feat: create new db migration for new features

---
+++ migrations/1721957896366_change-time-collumn-data-type-in-playlist-activities-table.cjs
@@ -0,0 +1,17 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.alterColumn('playlist_activities', 'time', { type: 'TIMESTAMPTZ' })
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.alterColumn('playlist_activities', 'time', { type: 'TIMESTAMP' })
+}
